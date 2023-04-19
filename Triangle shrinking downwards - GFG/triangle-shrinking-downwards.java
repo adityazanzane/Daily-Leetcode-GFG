a6ca1e27d1e9e19c9b14c28ea53b8da86a454426
Aditya Zanzane <91719677+adityazanzane@users.noreply.github.com>
2023-04-19 15:03:04 +0530

    Added solution - LeetHub

---
+++ Triangle shrinking downwards - GFG/triangle-shrinking-downwards.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+
+            Solution ob = new Solution();
+            
+            String ans = ob.triDownwards(S);
+            
+            for(int i=0 ; i<ans.length() ; i++)
+            {
+                System.out.print(ans.charAt(i));
+                if((i+1)%S.length()==0)
+                    System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static String triDownwards(String S){
+        // code here
+        StringBuilder sb=new StringBuilder();
+        sb.append(S);
+        for(int i=1;i<S.length();i++){
+            int j=0;
+            while(j<i){
+                sb.append(".");
+                j++;
+            }
+            sb.append(S, i, S.length());
+        }
+        return sb.toString();
+    }
+};
