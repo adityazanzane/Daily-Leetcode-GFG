ba434e48382d681efbff140e108fdff9fe2c05f1
Aditya Zanzane <91719677+adityazanzane@users.noreply.github.com>
2023-07-30 11:57:38 +0530

    Added solution - LeetHub

---
+++ Handshakes - GFG/handshakes.java
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.count(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int count(int N) 
+    { 
+       if(N==0) return 1;
+       int ans=0;
+       for(int i=0; i < N; i+=2)
+          ans+= count(i)*count(N-2-i);
+       
+       return ans;
+    }
+};
