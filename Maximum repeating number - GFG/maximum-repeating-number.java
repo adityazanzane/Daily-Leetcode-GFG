4306abe88adbf0106e1027daf77c38cbd13772b7
Aditya Zanzane <91719677+adityazanzane@users.noreply.github.com>
2023-10-21 09:10:03 +0530

    Added solution - LeetHub

---
+++ Maximum repeating number - GFG/maximum-repeating-number.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().maxRepeating(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    int maxRepeating(int[] arr, int n, int k) {
+        // code here
+        int[] freq = new int[k];
+    
+        for (int i = 0; i < n; i++){
+            freq[arr[i]]++;
+        }
+    
+        int max = freq[0];
+        int result = 0;
+        for (int i = 1; i < k; i++) {
+            if (freq[i] > max) {
+                max = freq[i];
+                result = i;
+            }
+        }
+        return result;
+    }
+}
