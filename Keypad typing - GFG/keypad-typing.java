59888eda3bbd362e3ba3eee9c47e0c6835361daf
Aditya Zanzane <91719677+adityazanzane@users.noreply.github.com>
2023-11-07 10:19:49 +0530

    Added solution - LeetHub

---
+++ Keypad typing - GFG/keypad-typing.java
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		sc.nextLine();
+		while(t-->0)
+		{
+		    String s=sc.nextLine();
+		    
+		    System.out.println(printNumber(s,s.length()));
+		}
+		
+	}
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+//Function to find matching decimal representation of a string as on the keypad.
+public static String printNumber(String s, int n) 
+{
+    //Your code here
+    int[] arr = new int[]{2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,9,9,9,9};
+    String ans = "";
+    for(int i=0; i<n; i++){
+        int t = arr[s.charAt(i)-'a'];
+        ans += t;
+    }
+    return ans;
+}
+
+//{ Driver Code Starts.
+
+}
+
+// } Driver Code Ends
