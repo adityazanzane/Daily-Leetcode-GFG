859823d24a59ecd047a5e5f0f9a93ff9a3f8c736
Aditya Zanzane <91719677+adityazanzane@users.noreply.github.com>
2023-11-08 10:37:53 +0530

    Added solution - LeetHub

---
+++ Find n/k th node in Linked list - GFG/find-nk-th-node-in-linked-list.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class LinkedList
+{
+    static Node head;
+    
+     public static void addToTheLast(Node node) 
+	{
+		if (head == null) 
+		{
+			head = node;
+		} else 
+		{
+		   Node temp = head;
+		   while (temp.next != null)
+		   temp = temp.next;
+		   temp.next = node;
+		}
+    }
+    
+     public static void main (String[] args) {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t-- > 0)
+		 {
+		     int n = sc.nextInt();
+		     int a1 = sc.nextInt();
+		     Node head = new Node(a1);
+		     addToTheLast(head);
+		     for(int i = 1; i < n; i++)
+		     {
+		         int a = sc.nextInt(); 
+				 addToTheLast(new Node(a));
+			 }
+			 int k = sc.nextInt();
+			GfG gfg = new GfG();
+			System.out.println(gfg.nknode(head, k));
+		     }
+		 }
+}
+// } Driver Code Ends
+
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class GfG
+{
+    public static int nknode(Node head, int k)
+    {
+       Node current=head;
+       int count=1;
+       while(current.next!=null){
+           count++;
+           current=current.next;
+       }
+       int th=(int)Math.ceil((double)count/k);
+       
+       count=1;
+       current=head;
+       while(current.next!=null){
+           
+           if(th==count){
+               return current.data;
+           }
+           current=current.next;
+           count++;
+       }
+       return current.data;
+    }
+}
